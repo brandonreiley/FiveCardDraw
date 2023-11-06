2524bf212360c9dab3b3e72fb075f815a1c2057f
Brandon Reiley <57162817+brandonreiley@users.noreply.github.com>
2023-11-06 15:55:44 -0800

    Update Main.java

--- Five Card Draw Program/src/Main.java
+++ Five Card Draw Program/src/Main.java
@@ -2,13 +2,27 @@
  *strength algorithm that needed the Player class so I then had to 
  *connect my card and createDeck class to each.
  */
+import java.util.Scanner;
 public class Main {
 	public static void main(String[] args) {
-			createDeck pokerDeck = new createDeck();
-			dealHand hand = new dealHand(pokerDeck);
-			//deck gets shuffled right after the deck arraylist is filled with card objects in createDeck class
-			System.out.println("Shuffling ... Shuffling ... Shuffling ...\n");
-			hand.currentHand(pokerDeck);
-			
+		boolean game = true;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Play game? y/n");
+
+		while(game == true) {
+			if(scan.nextLine().replaceAll("\\s+", "").equals("y")) {
+				createDeck pokerDeck = new createDeck();
+				dealHand hand = new dealHand(pokerDeck);
+				//deck gets shuffled right after the deck arraylist is filled with card objects in createDeck class
+				System.out.println("Shuffling ... Shuffling ... Shuffling ...\n");
+				hand.currentHand(pokerDeck);
+				System.out.println();
+				System.out.println("Play again? y/n");
+			}
+			else {
+				game = false;
+			}
+		}
+		scan.close();
 	}
 }
